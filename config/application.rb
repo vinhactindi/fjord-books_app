90094fc9c287b90cd3533f89ca62ddee35562ccb
vinhactindi <nguyen.the.vinh@actindi.net>
2021-10-21 16:13:53 +0700

    add i18n

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module BooksApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.i18n.available_locales = [:ja, :en]
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
