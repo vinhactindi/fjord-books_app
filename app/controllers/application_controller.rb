20308336eb2ed5915774d92b987b11d6742b2e78
vinhactindi <nguyen.the.vinh@actindi.net>
2021-10-25 14:38:31 +0700

    add profile (postal code, address, bio) field to devise registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    profile = %i[postal_code address bio]
+    devise_parameter_sanitizer.permit(:sign_up, keys: profile)
+    devise_parameter_sanitizer.permit(:account_update, keys: profile)
+  end
 end
