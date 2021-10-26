0501cce297abc8ccd0febf265a5e40364444bbca
vinhactindi <nguyen.the.vinh@actindi.net>
2021-10-26 14:05:48 +0700

    add some user follow method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@ class User < ApplicationRecord
 
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
