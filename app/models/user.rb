5a3dedcbd28c5c3734520ebbdf8f47fba9d80f53
vinhactindi <nguyen.the.vinh@actindi.net>
2021-10-26 14:28:52 +0700

    update routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   has_many :active_relationships, class_name: 'Following', foreign_key: 'follower_id', dependent: :destroy, inverse_of: :follower
-  has_many :passive_relationships, class_name: 'Following', foreign_key: 'followed_id', dependent: :destroy, inverse_of: :following
+  has_many :passive_relationships, class_name: 'Following', foreign_key: 'followed_id', dependent: :destroy, inverse_of: :followed
 
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
   def follow(other_user)
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books
-  resources :users, only: %i(index show)
+  resources :users, only: %i[index show] do
+    scope module: :users do
+      resources :followings, only: :index
+      resources :followers, only: :index
+    end
+  end
 end
