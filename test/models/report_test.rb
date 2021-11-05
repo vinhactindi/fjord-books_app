585fc8e6893c431d822fbab508941be4ea8bf0e4
vinhactindi <nguyen.the.vinh@actindi.net>
2021-11-05 10:43:27 +0700

    test for book and report

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    user = User.create!(email: 'foo@example.com', password: 'password')
+    report = Report.create!(user_id: user.id, title: 'Fancy title', content: 'This is a nice content')
+
+    assert report.editable?(user)
+  end
+
+  test '#created_on' do
+    user = User.create!(email: 'foo@example.com', password: 'password')
+    report = Report.create!(user_id: user.id, title: 'Fancy title', content: 'This is a nice content', created_at: '2021/11/05')
+
+    assert_equal Date.new(2021, 11, 5), report.created_on
+  end
 end
