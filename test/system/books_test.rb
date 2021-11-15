aa68cbb5dd00286dba62fa09096ff15ffabbafa4
vinhactindi <nguyen.the.vinh@actindi.net>
2021-11-15 09:48:15 +0700

    update system test for books

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,8 +4,6 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
-
     visit root_path
     fill_in 'Eメール', with: 'alice@example.com'
     fill_in 'パスワード', with: 'password'
@@ -21,24 +19,32 @@ class BooksTest < ApplicationSystemTestCase
     visit books_url
     click_on '新規作成'
 
-    fill_in 'メモ', with: @book.memo
-    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: 'Fancy book memo'
+    fill_in 'タイトル', with: 'The amazing title'
     click_on '登録する'
 
     assert_text '本が作成されました。'
+    assert_text 'Fancy book memo'
+    assert_text 'The amazing title'
+
     click_on '戻る'
+    assert_selector 'h1', text: '本'
   end
 
   test 'updating a Book' do
     visit books_url
     click_on '編集'
 
-    fill_in 'メモ', with: @book.memo
-    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: 'An updated memo'
+    fill_in 'タイトル', with: 'The updated amazing title'
     click_on '更新する'
 
     assert_text '本が更新されました。'
+    assert_text 'An updated memo'
+    assert_text 'The updated amazing title'
+
     click_on '戻る'
+    assert_selector 'h1', text: '本'
   end
 
   test 'destroying a Book' do
