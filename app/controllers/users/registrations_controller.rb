cc16c3d8fd1d2fc4390079bcd3a84f8b01819d1c
vinhactindi <nguyen.the.vinh@actindi.net>
2022-02-25 14:30:12 +0700

    パスワードを変更する場合は現在のパスワードの入力が必要

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
-    resource.update_without_password(params)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:current_password)
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
   end
 end
