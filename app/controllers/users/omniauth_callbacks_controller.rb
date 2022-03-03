e2972077915ca07895af43dbe80b780f43fd4de9
vinhactindi <nguyen.the.vinh@actindi.net>
2022-03-03 09:18:11 +0700

    ログインページにリダイレクト

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,8 +10,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Github')
     else
-      session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
-      redirect_to new_user_registration_url
+      redirect_to new_user_session_url('user[email]': request.env['omniauth.auth']['info']['email'])
     end
   end
 
