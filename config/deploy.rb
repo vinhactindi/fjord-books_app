b8d19b830ce5b23ae0ec29504bc9100bcfaddbdd
vinhactindi <nguyen.the.vinh@actindi.net>
2022-03-22 09:33:16 +0700

    LinkedFilesにdatabase.ymlの削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/master.key'
+append :linked_files, 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'public/system', 'vendor', 'storage'
