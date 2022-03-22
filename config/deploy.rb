87c48812d174d4c296a054e5b4c43ab46762003a
vinhactindi <nguyen.the.vinh@actindi.net>
2022-03-22 10:49:30 +0700

    pumaの設定を削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,36 +37,3 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-
-set :rbenv_type, :user
-
-set :rbenv_ruby_version, 'ruby-3.0.1' # Should match ruby version
-
-
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-
-set :puma_conf, "#{shared_path}/puma.rb"
-
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-
-set :puma_role, :app
-
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-
-set :puma_threads, [0, 8]
-
-set :puma_workers, 0
-
-set :puma_worker_timeout, nil
-
-set :puma_init_active_record, true
-
-set :puma_preload_app, false
