4f9d474a1affb7be5f91519d777144634e162f8f
vinhactindi <nguyen.the.vinh@actindi.net>
2022-03-23 10:24:03 +0700

    testとdevelopment host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener_web
+
+  config.hosts << '127.0.0.1'
+  config.hosts << 'localhost'
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  config.hosts << '127.0.0.1'
+  config.hosts << 'localhost'
 end
